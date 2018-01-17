4a6498ad5b218668ef3578ad74c8bb6fbc0bc610
Wouter van Marrum <wouter.van.marrum@protonmail.com>
2018-01-17 22:59:48 +0100

    Cleaning up the `TesseractServiceProvider` since we will need any of the following items : - routes - views - assets - migrations

--- src/TesseractServiceProvider.php
+++ src/TesseractServiceProvider.php
@@ -44,53 +44,9 @@ class TesseractServiceProvider extends ServiceProvider
     /**
      * @return $this
      */
-    private function copyRoutes()
-    {
-        $this->loadroutesFrom(__DIR__ . '/../routes/web.php');
-        return $this;
-    }
-
-    /**
-     * @return $this
-     */
-    private function copyViews()
-    {
-        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'Tesseract');
-
-        $this->publishes([
-            __DIR__ . '/../resources/views' => resource_path('views/vendor/Tesseract'),
-        ]);
-        return $this;
-    }
-
-    /**
-     * @return $this
-     */
-    private function copyAssets()
-    {
-        $this->publishes([
-            __DIR__ . '/resources/assets' => public_path('vendor/Tesseract'),
-        ], 'public');
-        return $this;
-    }
-
-    /**
-     * @return $this
-     */
     private function copyTranslations()
     {
         $this->loadTranslationsFrom(__DIR__ . '/../resources/translations', 'Tesseract');
-        return $this;
-    }
-
-    /**
-     * @return $this
-     */
-    private function copyMigrations()
-    {
-        $this->publishes([
-            __DIR__ . '/../database/migrations/' => database_path('migrations')
-        ], 'migrations');
         return $this;
     }
 }
