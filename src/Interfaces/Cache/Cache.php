2e323649f5bb10b78bd863001a55bc7ce5d4e905
Wouter van Marrum <wouter.van.marrum@protonmail.com>
2018-01-22 19:36:58 +0100

    - Added `Cache` object.

---
+++ src/Interfaces/Cache/Cache.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace ConceptCore\Tesseract\Interfaces\Cache;
+
+use Illuminate\Contracts\Cache\Store;
+
+interface Cache extends Store
+{
+
+}
---
+++ src/Objects/Cache/Cache.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace ConceptCore\Tesseract\Objects\Cache;
+
+use ConceptCore\Tesseract\Interfaces\Cache\Cache as CacheInterface;
+
+class Cache implements CacheInterface
+{
+
+}
